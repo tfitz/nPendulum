097d3775819ef00c43be2f531cbcb00dfd6c5402
tim <tfitz@umd.edu>
2019-03-13 17:28:02 -0700

    Test speed of Broyden Updates
    - Not good: slower than fixed-point iteration.  Will remove

--- src/main.jl
+++ src/main.jl
@@ -1,51 +1,32 @@
 using Pkg
 Pkg.activate(".")
 
-# include("nPendulum.jl")
-# using .nPendulum
+include("nPendulum.jl")
+using .nPendulum
+include("nPendulumAnimation.jl")
+using .nPendulumAnimation
+using Plots
 
 ## Parameters
 # number of pendulums:
-# N = 2
+N = 10
 # ending time:
-# tf = 0.1
+tf = 1.
+# frames per second in the animation
+fps = 24
+# number of frames in the animation
+num_frames = tf*fps*2 |> round |> Int
+
 
 ## Solve the system
-# p   = generateRandomParameters(N)
-# z0  = generateRandomInitialConfig(p)
-# z,t = solvePendulum(p, z0, tf, 0.01 )
+p   = generateRandomParameters(N)
+z0  = generateRandomInitialConfig(p)
+
+@elapsed z1,t1 = solvePendulum(p, z0, tf, 0.01, method=:broyden )
+
+@elapsed z2,t2 = solvePendulum(p,z0,tf,0.01, method=:fixedpoint )
 
 
-include("IRK.jl")
-using .IRK
-using LinearAlgebra
-z0 = [1.]
-h = 0.1
-tn = 0 + h
-f(t,x) = [ -x[1] ]
-# num_stages = 3
-# butcherTable = IRK.GaussLegendreRKRule.buildRule(num_stages)
-# iter_max = 500
-# tol = 1e-8
-# n  = length(z0)
-# s  = butcherTable.num_stages
-# K  = zeros( Float64, n, s )
-# # Make an explicit guess
-# xn = z0
-# for i = 1:s
-#     K[:,i] = f( tn+h*butcherTable.c[i], xn )
-# end
-#
-# # fixed point iteration until convergence
-# # K, flag, rel_err = fixed_point_iteration(f, xn, tn, K, h, butcherTable, iter_max, tol)
-# invJ0 = I
-# K, invJ0, flag, rel_err,iter = IRK.broyden_iteration(f, invJ0, xn, tn, K, h, butcherTable, iter_max, tol)
-
-
-z1,t1 = IRK.IRKadapt( f, z0, 0., 0.02, 0.01)
-println(z1)
-println(t1)
-
-z2,t2 = IRK.IRKfixed( f, z0, 0., 0.02, 0.01)
-println(z2)
-println(t2)
+## Generate the animation
+# anim = GenPendulumAnimation(t,z,num_frames,p)
+# gif(anim, "./anim.gif", fps = fps)
